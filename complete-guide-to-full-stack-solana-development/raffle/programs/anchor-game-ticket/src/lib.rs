cd56ecc412de6246e527a1be38a791d0ab2b4c7a
Piotr <piotrszeliga45@gmail.com>
2022-08-24 22:23:22 +0300

    - Program and typescript improvements

--- complete-guide-to-full-stack-solana-development/raffle/programs/anchor-game-ticket/src/lib.rs
+++ complete-guide-to-full-stack-solana-development/raffle/programs/anchor-game-ticket/src/lib.rs
@@ -1,22 +1,26 @@
 use anchor_lang::prelude::*;
 use anchor_lang::system_program::{transfer, Transfer};
+//use anchor_spl::{self, associated_token::{AssociatedToken}, token::{self, Mint, TokenAccount, Token}};
 
 declare_id!("AGyQHJtznL3WiqWzsV31Rxpvvk4qwZHnaUVn9LPdnjZj");
 
 pub const PRICE_PER_TICKET: u64 = 150_000_000;
+pub const LAMPORTS_PER_SOL: u64 = 1000000000;
 
 #[program]
-pub mod anchor_game_ticket {
+pub mod anchor_raffle_ticket {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, amount: u32) -> Result<()> {
-        let game = &mut ctx.accounts.game;
-        game.total_tickets = amount;
-        game.sold_tickets = 0;
+    pub fn initialize(ctx: Context<Initialize>, price: u64, amount: u32) -> Result<()> {
+        let raffle = &mut ctx.accounts.raffle;
+        raffle.price_per_ticket = price;
+        raffle.total_tickets = amount;
+        raffle.sold_tickets = 0;
 
         msg!("Program initialized successfully.");
-        msg!("Total Tickets: {:?}", game.total_tickets);
-        msg!("Sold Tickets: {:?}", game.sold_tickets);
+        msg!("Total Tickets: {:?}", raffle.total_tickets);
+        msg!("Sold Tickets: {:?}", raffle.sold_tickets);
+        msg!("Price Per Ticket: {} {}", raffle.price_per_ticket, raffle.price_per_ticket as f64 / LAMPORTS_PER_SOL as f64);
 
         Ok(())
     }
@@ -30,29 +34,41 @@ pub mod anchor_game_ticket {
             },
         );
 
+        let raffle = &mut ctx.accounts.raffle;
+
+        //transfer(cpi_context, raffle.price_per_ticket * LAMPORTS_PER_SOL * amount as u64)?;
         transfer(cpi_context, PRICE_PER_TICKET * amount as u64)?;
 
-        let game = &mut ctx.accounts.game;
-        game.sold_tickets = game.sold_tickets.checked_add(amount).unwrap();
-        
+        raffle.sold_tickets = raffle.sold_tickets.checked_add(amount).unwrap();
+
         emit!(BuyEvent
             {
                 buyer: *ctx.accounts.buyer.to_account_info().key,
                 amount: amount,
-                sold_tickets: game.sold_tickets,
-                total_tickets: game.total_tickets,
-                remaining_tickets: game.total_tickets - game.sold_tickets
+                sold_tickets: raffle.sold_tickets,
+                total_tickets: raffle.total_tickets,
+                remaining_tickets: raffle.total_tickets - raffle.sold_tickets
             });
 
 
         msg!("Buyer: {:?}", *ctx.accounts.buyer.to_account_info().key);
         msg!("Buy Amount: {:?}", amount);
-        msg!("Total Tickets: {:?}", game.total_tickets);
-        msg!("Sold Tickets: {:?}", game.sold_tickets);
-        msg!("Remaining Tickets: {:?}", game.total_tickets - game.sold_tickets);
+        msg!("Total Tickets: {:?}", raffle.total_tickets);
+        msg!("Sold Tickets: {:?}", raffle.sold_tickets);
+        msg!("Remaining Tickets: {:?}", raffle.total_tickets - raffle.sold_tickets);
 
         Ok(())
     }
+
+    // pub fn create_mint_and_vault(ctx: Context<Initialize>, _decimals: u8, amount: u64) -> Result<T> {
+    //     msg!("create_mint_and_vault begin");
+    //     let mint_to_ctx = token::MintTo {
+    //         mint: ctx.accounts.mint.to_account_info(),
+    //         to: ctx.accounts.vault.to_account_info(),
+    //         authority: ctx.accounts.authority.to_account_info()
+    //     };
+    //     return token::mint_to(CpiContext::new(ctx.accounts.token_program.to_account_info(), mint_to_ctx), amount);
+    // }
 }
 
 #[derive(Accounts)]
@@ -61,8 +77,8 @@ pub struct Initialize<'info> {
     #[account(mut)]
     payer: Signer<'info>,
     // raffle
-    #[account(init, payer = payer, space = Game::LEN + 8)]
-    game: Account<'info, Game>,
+    #[account(init, payer = payer, space = Raffle::LEN + 8)]
+    raffle: Account<'info, Raffle>,
     // system program
     system_program: Program<'info, System>,
 }
@@ -78,20 +94,21 @@ pub struct BuyTicket<'info> {
     #[account(mut)]
     recipient: AccountInfo<'info>,
     // raffle
-    #[account(mut, constraint = amount + game.sold_tickets <= game.total_tickets)]
-    game: Account<'info, Game>,
+    #[account(mut, constraint = amount + raffle.sold_tickets <= raffle.total_tickets)]
+    raffle: Account<'info, Raffle>,
     // system program
     system_program: Program<'info, System>,
 }
 
 #[account]
-pub struct Game {
+pub struct Raffle {
     pub total_tickets: u32,
     pub sold_tickets: u32,
+    pub price_per_ticket: u64,
 }
 
-impl Game {
-    pub const LEN: usize = 4 + 4;
+impl Raffle {
+    pub const LEN: usize = 4 + 4 + 8;
 }
 
 #[event]
--- complete-guide-to-full-stack-solana-development/raffle/tests/anchor-game-ticket.ts
+++ complete-guide-to-full-stack-solana-development/raffle/tests/anchor-game-ticket.ts
@@ -1,7 +1,7 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 // @ts-ignore
-import { AnchorGameTicket } from "../target/types/anchor_game_ticket";
+import { AnchorRaffleTicket } from "../target/types/anchor_raffle_ticket";
 import {
   Keypair,
   PublicKey,
@@ -9,7 +9,7 @@ import {
 } from '@solana/web3.js';
 
 async function spawnMoney(
-  program: anchor.Program<AnchorGameTicket>,
+  program: anchor.Program<AnchorRaffleTicket>,
   to: PublicKey,
   sol: number
 ): Promise<anchor.web3.TransactionSignature> {
@@ -23,6 +23,10 @@ async function spawnMoney(
       toPubkey: to,
     })
   );
+
+  // @ts-ignore
+  console.log(`Sending SOL: ${program.provider.wallet.publicKey.toString()} sent ${sol} to ${to.toString()} `);
+
   return await program.provider.sendAndConfirm(transaction, [], {
     commitment: "confirmed",
   });
@@ -32,13 +36,14 @@ describe("anchor-game-ticket", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.AnchorGameTicket as Program<AnchorGameTicket>;
+  const program = anchor.workspace.AnchorRaffleTicket as Program<AnchorRaffleTicket>;
 
   it("Is initialized!", async () =>
   {
-    console.log("START");
+    console.log("\n\nSTART:\n");
     // @ts-ignore
-    console.log("Program Wallet: ", program.provider.wallet.publicKey.toString());
+    console.log("Program Wallet:", program.provider.wallet.publicKey.toString());
+
     // Fund me
     // const airdropSignature = await program.provider.connection.requestAirdrop(
     //     new PublicKey("6rqb63zZ2YgNty5QRz9PLFbN1oPxZdZaP5TwG4vtWHsQ"),
@@ -47,26 +52,34 @@ describe("anchor-game-ticket", () => {
     // await new Promise(f => setTimeout(f, 1000));
 
     // Add your test here.
-    const receiver = Keypair.generate();
-    console.log("receiver: ", receiver.publicKey.toString());
-    await spawnMoney(program, receiver.publicKey, 0.05);
-    console.log("Step-1");
+    //const receiver = Keypair.generate();
+    const receiver = Keypair.fromSecretKey(new Uint8Array([133,230,105,82,126,147,188,49,144,121,98,112,160,239,106,142,105,92,58,193,34,169,161,57,57,231,154,146,19,17,244,172,16,123,70,229,190,105,161,60,53,123,148,82,214,237,122,193,24,62,101,168,243,70,149,117,33,159,75,104,193,83,97,231]));
+    console.log("receiver:", receiver.publicKey.toString());
+    //console.log(receiver.secretKey.toString());
+    await spawnMoney(program, receiver.publicKey, 0.1);
 
-    //const raffle = Keypair.generate();
-    const raffle = Keypair.fromSecretKey(new Uint8Array([124,211,103,193,156,71,237,41,91,34,3,54,153,166,248,82,235,87,57,59,157,116,252,83,224,65,202,118,152,6,228,75,209,212,209,22,216,40,227,169,105,1,33,119,247,42,110,129,130,187,35,115,231,56,130,83,2,155,213,235,40,91,92,40]));
+    console.log("Step-1 DONE");
+
+    // const raffle = Keypair.generate();
+    const raffle = Keypair.fromSecretKey(new Uint8Array([159,238,176,226,53,7,139,231,65,16,97,112,122,138,104,181,255,203,13,132,68,226,195,28,103,159,236,14,230,40,149,88,18,176,59,241,187,184,125,217,91,112,78,227,125,55,22,153,76,159,3,170,89,221,76,214,152,23,169,204,50,216,31,87]));
     console.log("raffle :", raffle.publicKey.toString());
     console.log(raffle.secretKey.toString());
 
-    const account = await program.account.game.fetchNullable(raffle.publicKey);
-    console.log('account exists: ', account);
+    const account = await program.account.raffle.fetchNullable(raffle.publicKey);
+    // @ts-ignore
+    account.pricePerTicketSOL = account.pricePerTicket.toNumber() / anchor.web3.LAMPORTS_PER_SOL + " Sol";
+    console.log('account exists:', account);
 
     if (!account)
     {
       // @ts-ignore
-      await program.rpc.initialize(5, {
+
+      const price = 3.2;
+      await program.rpc.initialize(new anchor.BN(price * anchor.web3.LAMPORTS_PER_SOL), 7,
+       {
         accounts: {
           payer: receiver.publicKey,
-          game: raffle.publicKey,
+          raffle: raffle.publicKey,
           systemProgram: SystemProgram.programId,
         },
         signers: [receiver, raffle],
@@ -76,7 +89,9 @@ describe("anchor-game-ticket", () => {
       });
     }
 
-    console.log("Step-2");
+    console.log("Step-2 DONE");
+    return;
+
     const buyer = Keypair.generate();
     console.log("buyer: ", buyer.publicKey.toString());
     await spawnMoney(program, buyer.publicKey, .2);
@@ -91,7 +106,7 @@ describe("anchor-game-ticket", () => {
       accounts: {
         buyer: buyer.publicKey,
         recipient: receiver.publicKey,
-        game: raffle.publicKey,
+        raffle: raffle.publicKey,
         systemProgram: SystemProgram.programId,
       },
       signers: [buyer],
