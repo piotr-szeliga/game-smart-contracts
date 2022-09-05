899c802585052ed0249b391b6904966c07435f17
Piotr <piotrszeliga45@gmail.com>
2022-09-05 15:26:10 +0300

    - Cleanup #1

--- Token-Creator-master-npm-run-dev/src/views/index.tsx
+++
@@ -1,5 +0,0 @@
-export { HomeView } from "./home";
-export { MetadataView } from './metadata';
-export { UploaderView } from './uploader';
-export { BasicsView } from "./basics";
-export { UpdateView } from "./update";
--- my_sol_data/programs/my_sol_data/src/state.rs
+++
@@ -1,8 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[account]
-pub struct MyData {
-    pub number : u8,
-    pub message : String,
-    pub owner : Pubkey,
-}
