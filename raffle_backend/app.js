75e33b752a3fde4ce63b1a55a7bb74067211790a
Piotr <piotrszeliga45@gmail.com>
2022-09-25 21:25:20 +0500

    test using switchboard

--- raffle_backend/app.js
+++ raffle_backend/app.js
@@ -1,9 +1,26 @@
 const app = require("./config/express");
 const txCtrl = require("./controller/transaction.controller");
-const { Keypair } = require('@solana/web3.js');
-const bs58 = require('bs58');
+const { Keypair } = require("@solana/web3.js");
+const bs58 = require("bs58");
+const anchor = require("@project-serum/anchor");
+const {
+  loadSwitchboardProgram,
+  VrfAccount,
+} = require("@switchboard-xyz/switchboard-v2");
 
-app.get('/transaction/:clientKey', txCtrl.getTransaction);
+let payer = Keypair.fromSecretKey(
+  bs58.decode("")
+);
+const program = await loadSwitchboardProgram("devnet", undefined, payer);
+
+const vrfAccount = new VrfAccount({
+  program,
+  publicKey: vrfKey,
+});
+const vrf = await vrfAccount.loadData();
+console.log(vrf.currentRound.result);
+
+app.get("/transaction/:clientKey", txCtrl.getTransaction);
 
 // const kp = Keypair.generate();
 // console.log(kp.publicKey.toString());
